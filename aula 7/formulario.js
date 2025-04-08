681d733f1b81027b6a5aacd3128291577b77a30b
EndryoBittencourt <endryo.jogos@outlook.com>
2025-04-08 17:15:10 -0300

    formulario js

---
+++ aula 7/formulario.js
@@ -0,0 +1,64 @@
+//EXECUTAR MASCARAS
+
+//DEFINE O OBJETO E CHAMA A FUNCAO
+function mascara(o, f) {
+    Object=o
+    funcao=f
+    setTimeout ("executaMascara",1)
+}
+
+function executaMascara(){
+    objeto.value=funcao(objeto.value)
+}
+
+//Mascara de Telefone
+function telefone(variavel){
+
+
+    variavel = variavel.replace(/\D/g,"")
+
+
+    variavel = variavel.replace(/^(\d\d)(\d)/g,"($1) $2")
+
+
+    variavel = variavel.replace(/(\d{4})(\d)/,"$1-$2")
+    return variavel
+    }
+
+//Mascara do RG e CPF
+function RGeCPF(variavel){
+
+
+    variavel = variavel.replace (/\D/g,"")
+
+    //coloca um ponto apos o terceiro digito e o quarto
+    variavel = variavel.replace(/(\d{3})(\d)/,"$1-$2")
+
+    //coloca um ponto apos o sexto digito e o setimo
+    variavel = variavel.replace(/(\d{3})(\d)/,"$1-$2")
+
+    //coloca o hifem apos o setimo digito e permite apenas 2 digitos apos o hifem
+    variavel = variavel.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return variavel
+
+}
+
+//mascara do cep
+function cep(variavel){
+    variavel = variavel.replace(/\D/g,"") //remove tudo o que nao é digito
+    variavel = variavel.replace(/(\d{2})(\d)/,"$1.$2") //coloca 2 barra
+    variavel = variavel.replace(/(\d{3})(\d{1,3})$/,"$1-$2")
+    return variavel
+
+}
+
+//mascara data
+function data(variavel){
+    variavel = variavel.replace(/\D/g,"")
+    variavel = variavel.replace(/(\d{2})(\d)/,"$1/$2")
+    variavel = variavel.replace(/(\d{2})(\d)/,"$1/$2")
+    return variavel
+
+}
+
+
