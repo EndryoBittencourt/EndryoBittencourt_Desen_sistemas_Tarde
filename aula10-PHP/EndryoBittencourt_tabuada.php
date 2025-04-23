547f269348daf79d23fd3ca74d32447b40302af4
EndryoBittencourt <endryo.jogos@outlook.com>
2025-04-23 15:19:24 -0300

    exercicio tabuada

--- aula10-PHP/EndryoBittencourt_tabuada.php
+++ aula10-PHP/EndryoBittencourt_tabuada.php
@@ -3,27 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabuada PHP</title>
+    <title>Tabuada Em PHP</title>
     <style type="text/css">
-        table {
-            border-collapse: collapse;
-        }
 
         th, td {
             border: solid;
-            width: 100px;
+            width: 85px;
         }
+
     </style>
 </head>
 <body>
-    <h1 align="center">Endryo Gabriel Bittencourt</h1>
+    <h1>Endryo Gabriel Bittencourt</h1>
 
     <table>
     <?php
+
         for ($x = 1; $x <= 10; $x++) {
-            echo "<tr>";
-            for ($y = 1; $y <= 10; $y++) {
-                echo "<td> $x x $y = ".$x*$y."</td>";
+
+         echo "<tr>";
+
+         for ($y = 1; $y <= 10; $y++) {
+
+             echo "<td> $x x $y = ".$x*$y."</td>";
+
             }
             echo "</tr>";
         }
